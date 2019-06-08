20ba8c653078e0e53ec5446b495f64d81848bb74
Alexandre Silva <alexandresilva933@gmail.com>
2019-06-08 18:18:11 -0300

    Configurações iniciais do projeto com create-react-app

---
+++ frontend/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default props => (
+    <div className="container">
+        <div className="row">
+            <div className="col-md-4">
+                <h1>Olá, Mundo!!!!!</h1>
+            </div>
+        </div>
+    </div>
+);
+
---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import Header from './Header';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+
+ReactDOM.render(
+    <Header />,
+    document.getElementById('root')
+)
