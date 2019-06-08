6a7d4d563e8e69d925c08e592dfeb9532257df70
Alexandre Silva <alexandresilva933@gmail.com>
2019-06-08 18:48:12 -0300

    Adição do bootstrap js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import Header from './Header';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.js'
 
 ReactDOM.render(
     <Header />,
