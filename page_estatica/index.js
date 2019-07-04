bb3e738a024eb3da9e4d9075dfcc7c3bc3f0574f
Bruna Lemos <mathaus_soares07@hotmail.com>
2019-07-04 19:42:29 -0300

    atualização do css e adição de algumas imagens

--- page_estatica/index.js
+++ page_estatica/index.js
@@ -4,13 +4,14 @@ function btn_menu_patrimonio() {
 }
 function scroll_line() {
     let line_down = document.getElementById("line_down");
-
+    let line_down_1 = document.getElementById("line_down_1");
     let start = Date.now();
+    console.log(start);
     let timer = setInterval(function () {
-
         let timePassed = Date.now() - start;
         line_down.style.height = timePassed / 5 + 'px';
-        if (timePassed > 1900) {
+        if (timePassed > 2600) {
+            document.getElementById("data-patrimonio").style.opacity = 1;
             clearInterval(timer);
         };
     }, 20);
