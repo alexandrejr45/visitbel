38cd80709e96112f3198a8ec032ad8079548284f
Alexandre Silva <alexandresilva933@gmail.com>
2019-07-04 01:01:11 -0300

    Alteração no arquivo de serviceWorker. Adição de um limite de cache para imagens e carregamento do javascript conforme a conexão

--- spa_patrimonio/public/service-worker.js
+++ spa_patrimonio/public/service-worker.js
@@ -10,8 +10,30 @@ if (workbox) {
 
 self.addEventListener("fetch", event => {
   if (event.request.url.endsWith(/\.(?:png|jpg|jpeg|svg|gif|css|js|html|ttf)$/)) {
-    // Oops! This causes workbox-strategies.js to be imported inside a fetch handler,
-    // outside of the initial, synchronous service worker execution.
+    workbox.routing.registerRoute(
+      /\.(?:js|css)$/,
+      new workbox.strategies.NetworkFirst()
+    );
+
+
+    workbox.routing.registerRoute(
+      // Cache image files.
+      /\.(?:png|jpg|jpeg|svg|gif|ttf)$/,
+      // Use the cache if it's available.
+      new workbox.strategies.CacheFirst({
+        // Use a custom cache name.
+        cacheName: "image-cache",
+        plugins: [
+          new workbox.expiration.Plugin({
+            // Cache only 20 images.
+            maxEntries: 20,
+            // Cache for a maximum of a week.
+            maxAgeSeconds: 7 * 24 * 60 * 60
+          })
+        ]
+      })
+    );
+
     const cacheFirst = new workbox.strategies.CacheFirst();
     event.respondWith(cacheFirst.makeRequest({ request: event.request }));
   }
