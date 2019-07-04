22a6be6e4972a303801061babe970d1c7c6e8532
Bruna Lemos <mathaus_soares07@hotmail.com>
2019-07-04 15:36:26 -0300

    atualização de arquivos de imagem e na folha de estilo

--- page_estatica/index.js
+++ page_estatica/index.js
@@ -1,4 +1,18 @@
-function btn_menu_patrimonio(){
+function btn_menu_patrimonio() {
     let btn_menu = document.getElementById("call-menu");
     btn_menu.classList.toggle("background-menu");
 }
+function scroll_line() {
+    let line_down = document.getElementById("line_down");
+
+    let start = Date.now();
+    let timer = setInterval(function () {
+
+        let timePassed = Date.now() - start;
+        line_down.style.height = timePassed / 5 + 'px';
+        if (timePassed > 1900) {
+            clearInterval(timer);
+        };
+    }, 20);
+}
+
