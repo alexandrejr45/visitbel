858626e2fab15ac4e62fdc0f4a957ead7d0900b8
Alexandre Silva <alexandresilva933@gmail.com>
2019-10-30 01:49:04 -0300

    Alteração no modo de exportação da função principal

--- frontend/spa_patrimonio/src/components/index.js
+++ frontend/spa_patrimonio/src/components/index.js
@@ -1,23 +1,21 @@
-import React, {Fragment} from "react";
-import { Redirect, Route, Switch } from "react-router";
-import Home from "./home/Home";
-import HomePatrimonio from "./patrimonio/HomePatrimonio";
-import Patrimonio from "./patrimonio/Patrimonio";
+import React, { Fragment } from 'react';
+import { Redirect, Route, Switch } from 'react-router';
+import Home from './home/Home';
+import HomePatrimonio from './patrimonio/HomePatrimonio';
+import Patrimonio from './patrimonio/Patrimonio';
 
-const Main = () => {
+export default function Main() {
   return (
     <Fragment>
       <main>
-          <Switch>
-            <Route exact path="/home" component={Home} />
-            <Route exact path="/patrimonio" component={HomePatrimonio} />
-            <Route path="/patrimonio" component={Patrimonio} />
-            <Route path="/patrimonio/:id/:option" component={Patrimonio} />
-            <Redirect from="*" to="/home" />
-          </Switch>
+        <Switch>
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/patrimonio" component={HomePatrimonio} />
+          <Route path="patrimonio/:id" component={Patrimonio}>
+          </Route>
+          <Redirect from="*" to="/home" />
+        </Switch>
       </main>
     </Fragment>
   );
-};
-
-export default Main;
+}
