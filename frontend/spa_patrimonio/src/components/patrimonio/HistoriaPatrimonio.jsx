7cfcda80f9f4b492f0298cfc9f89ff58dab7d269
matthaus <matthaus.brandao@pa.sebrae.com.br>
2019-10-31 23:13:16 -0300

    Modificação da page história e criação da timeline

--- frontend/spa_patrimonio/src/components/patrimonio/HistoriaPatrimonio.jsx
+++ frontend/spa_patrimonio/src/components/patrimonio/HistoriaPatrimonio.jsx
@@ -1,6 +1,7 @@
 import "../../css/pagina_patrimonio.css";
-import React, { Fragment } from "react";
+import imgSe from '../../img/igreja_se/img-se-2.jpg';
 
+import React, { Fragment } from "react";
 export default function HistoriaPatrimonio(props) {
   return (
     <Fragment>
@@ -9,17 +10,71 @@ export default function HistoriaPatrimonio(props) {
           <div className="col-12">
             <h3 className="ajust-title-conteudo text-center">História</h3>
           </div>
+          <div className="timeline">
+            <div className="timeline_contanin left">
+              <div className="content">
+                <img
+                  src={imgSe}
+                  className="shadow-img img-fluid right"
+                  width="250px"
+                  height="250px"
+                  alt="Homens no telhado da igreja da Sé no ano de 1950"
+                />
+                <h2>2017</h2>
+                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+              </div>
+            </div>
+            <div className="timeline_contanin right">
+              <div className="content">
+              <img
+                  src={imgSe}
+                  className="shadow-img img-fluid"
+                  width="250px"
+                  height="350px"
+                  alt="Homens no telhado da igreja da Sé no ano de 1950"
+                />
+                <h2>2016</h2>
+                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+              </div>
+            </div>
+            <div className="timeline_contanin left">
+              <div className="content">
+              <img
+                  src={imgSe}
+                  className="shadow-img img-fluid"
+                  width="250px"
+                  height="350px"
+                  alt="Homens no telhado da igreja da Sé no ano de 1950"
+                />
+                <h2>2015</h2>
+                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+              </div>
+            </div>
+            <div className="timeline_contanin right">
+              <div className="content">
+              <img
+                  src={imgSe}
+                  className="shadow-img"
+                  width="250px"
+                  height="350px"
+                  alt="Homens no telhado da igreja da Sé no ano de 1950"
+                />
+                <h2>2012</h2>
+                <p>Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam.</p>
+              </div>
+            </div>
+          </div>
+          {/* 
           {props.conteudo.anos.map((ano, index) => {
             return (
-              <div className="row" key={props.conteudo.id[index]}>
-                <div className="col-lg-5" style={{ marginTop: 100 + "px" }}>
+              <div  className="timeline" key={props.conteudo.id[index]}>
+                <div class>
                   <p style={{ color: "#707070", textAlign: "justify" }}>
                     {props.conteudo.historia}
                   </p>
                 </div>
                 <div className="col-lg-2">
                   <h3 className="ajust-title-conteudo text-center">{ano}</h3>
-                  <div id="line_down" className="add-line" />
                 </div>
                 <div
                   className="col-lg-5  text-center"
@@ -35,9 +90,17 @@ export default function HistoriaPatrimonio(props) {
                     />
                   </div>
                 </div>
+                <div
+                  className="col-lg-5 text-center"
+                  style={{ marginTop: 100 + 'px' }}>
+                  <p style={{ color: "#707070", textAlign: "justify" }}>
+                    {props.conteudo.historia}
+                  </p>
+
+                </div>
               </div>
             );
-          })}
+          })} */}
         </div>
       </section>
       <div style={{ marginTop: 3 + "em" }} />
--- frontend/spa_patrimonio/src/components/patrimonio/MenuPatrimonio.jsx
+++ frontend/spa_patrimonio/src/components/patrimonio/MenuPatrimonio.jsx
@@ -6,6 +6,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
@@ -16,7 +17,7 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
-
+import Patrimonio from './Patrimonio'
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
@@ -114,11 +115,17 @@ export default function MiniDrawer() {
           >
             <MenuIcon />
           </IconButton>
+          <Typography variant="h6" noWrap>
+            Mini variant drawer
+          </Typography>
         </Toolbar>
       </AppBar>
       <Drawer
         variant="permanent"
-   
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
         classes={{
           paper: clsx({
             [classes.drawerOpen]: open,
@@ -151,6 +158,14 @@ export default function MiniDrawer() {
           ))}
         </List>
       </Drawer>
+     
     </div>
   );
 }
+
+// MiniDrawer.propTypes = {
+//   classes: PropTypes.object.isRequired,
+//   theme: PropTypes.object.isRequired
+// };
+
+// export default withStyles(styles, { withTheme: true })(MiniDrawer);
